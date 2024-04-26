c8c705be15c3a60ed0832e96d5b920c436ab3416
leejlaa <lejladoric00@gmail.com>
2024-04-26 14:38:21 +0200

    second derivative and three point difference

--- aftermidterm/differentiation_error.jl
+++ aftermidterm/differentiation_error.jl
@@ -20,3 +20,21 @@ end
 
 println(central_difference(f, x, 0.00001))
 
+function three_point_forward_difference(f, x, h)
+    return (-3*f(x) + 4*f(x + h) - f(x + 2h)) / (2 * h)
+end
+
+println(three_point_forward_difference(f, x, 0.00001))
+
+function  three_point_backward_difference(f,x,h)
+    return (3*f(x) - 4*f(x - h) + f(x - 2h)) / (2 * h) 
+end
+
+println(three_point_backward_difference(f, x, 0.00001))
+
+function second_derivative(f, x, h)
+    return (f(x + h) - 2*f(x) + f(x - h)) / h^2
+end
+
+println(second_derivative(f, x, 0.00001))
+
